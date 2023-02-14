6c30bb8edd289214786f0d80b6d574d188230f0e
Eugene2538 <leugeniatc@gmail.com>
2023-02-13 21:12:28 -0500

    Se agrega posibilidad de cambiar de fuente de la foto

--- lib/src/home/ui/widgets/add_place_screen/body/add_new_place_photo.dart
+++ lib/src/home/ui/widgets/add_place_screen/body/add_new_place_photo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:platzi_trips_app/src/common_used_widgets/pretty_inkwell_button.dart';
 
 import '../../../../../common_used_widgets/action_button_generic.dart';
 import '../../../../../common_used_widgets/card_image_generic.dart';
@@ -47,24 +48,7 @@ class _AddNewPlacePhoto extends State<AddNewPlacePhoto> {
 
           ),
           ActionButtonGeneric(
-            onPressed: () async {
-              await ImagePicker().pickImage(
-                  source: ImageSource.camera
-              ).then((image) {
-                if (image == null) {
-                  return;
-                }
-
-                setState(() {
-                  photoUrl = image.path;
-                  imageNull = false;
-                  asset = false;
-
-                });
-
-
-              }).catchError((onError) => print(onError));
-            },
+            onPressed: (){showPickDialog(context);},
             heroTag: "Take a photo",
             icon: Icons.camera_alt,
             mini: false,
@@ -75,6 +59,66 @@ class _AddNewPlacePhoto extends State<AddNewPlacePhoto> {
 
         ]
     );
+  }
+
+  Future<void> showPickDialog(BuildContext context) async{
+    return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+
+            title: const Expanded(
+              child: Center(
+                child:Text(
+                  textAlign: TextAlign.center,
+                  'Take a photo or choose from your gallery',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16
+                  ),
+                ),
+              )
+            ) ,
+            content: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PrettyInkWellButton(
+                    onPressed: () async => pickImage(context, await ImagePicker().pickImage(source: ImageSource.camera)),
+                    textOfFilledButton: 'Camera',
+                    height: 50,
+                    width:  120,
+                    buttonWithIcon: false,
+                    snackbar: false
+                ),
+                const SizedBox(width: 20),
+                PrettyInkWellButton(
+                    onPressed: () async => pickImage(context, await ImagePicker().pickImage(source: ImageSource.gallery)),
+                    textOfFilledButton: 'Gallery',
+                    height: 50,
+                    width: 120,
+                    buttonWithIcon: false,
+                    snackbar: false
+                ),
+              ],
+            )
+          );
+        }
+    );
+  }
+
+  void pickImage(BuildContext context, image) {
+
+    Navigator.pop(context);//Cierra el dialog
+    if (image == null) {
+      return;
+    }
+
+    setState(() {
+      photoUrl = image.path;
+      imageNull = false;
+      asset = false;
+    });
+
   }
 }
 
